98e6a393c136ed7b45793f3bc7ca3c171215d329
sundarapandian <sure.game.studio@gmail.com>
2016-12-31 08:53:14 +0530

    + Initial Commit

---
+++ trunk/Assets/scripts/game/GameController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using thelab.mvc;
+using UnityEngine.SceneManagement;
+
+public class GameController : Controller<Game>
+{
+
+}
---
+++ trunk/Assets/scripts/game/GameView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using thelab.mvc;
+using UnityEngine.Events;
+
+public class GameView : View<Game>
+{
+}
